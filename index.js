85cb79b0dc413b7315fd34c7992a191bb904a8fa
Your Name <youremail@example.com>
2023-06-25 17:06:44 +0300

    commit message

--- index.js
+++ index.js
@@ -15,3 +15,29 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+function fetchBooks() {
+  const url = 'https://anapioficeandfire.com/api/books';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderBooks(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+function renderBooks(books) {
+  const bookTitles = books.map((book) => book.title);
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
+
+  bookTitles.forEach((title) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = title;
+    bookList.appendChild(listItem);
+  });
+}
+
+fetchBooks();
+
